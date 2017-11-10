282bb2cb57afdf5219a18a71f3cc0f0e91e0b2eb
allenyllee <allen7575@gmail.com>
2017-11-11 01:18:41 +0800

    add download script

---
+++ dataset2/download_dataset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# download fetch_gdrive_file.sh script
+# How do I get the raw version of a gist from github? - Stack Overflow 
+# https://stackoverflow.com/questions/16589511/how-do-i-get-the-raw-version-of-a-gist-from-github
+# 
+# If you want the last version of a Gist document, just remove the <commit>/ from URL
+# https://gist.githubusercontent.com/VonC/9184693/raw/testNewGist
+#
+# allows you do non-interactively download large public files from gdrive 
+# https://gist.github.com/allenyllee/ac42730e9c756e0aeb9b2f07424b68dd
+# 
+wget https://gist.githubusercontent.com/allenyllee/ac42730e9c756e0aeb9b2f07424b68dd/raw/fetch_gdrive_file.sh -O fetch_gdrive_file.sh
+
+URL=$1
+
+# if file KDEF.7z doesn't exist, then download it
+if ! [ -e KDEF.7z ]; then
+    # download KDEF.7z
+    ./fetch_gdrive_file.sh $URL KDEF.7z
+else
+    echo "remove KDEF.7z..."
+    echo "please execute this script again"
+    rm -rf KDEF.7z
+fi
+
+# if folder KDEF doesn't exist, then extract it
+if ! [ -d KDEF ]; then
+    # un7zip
+    7z x KDEF.7z
+else
+    echo "remove KDEF..."
+    echo "please execute this script again"
+    rm -rf KDEF.7z
+fi
